605be045fc9ba887677bc4192c268e05ebd6a82f
Varun Panchal <varunpanchal1008@gmail.com>
2026-05-02 15:25:59 +0530

    update

--- db.py
+++ db.py
@@ -1,8 +1,10 @@
-import os
+
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "mysql+pymysql://3fJbKsdTZJN4psR.root:P4rWNqS9tDrBr5vm@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/test?ssl_ca=<CA_PATH>&ssl_verify_cert=true&ssl_verify_identity=true"
 
 engine = create_engine(
     DATABASE_URL,
