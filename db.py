58c6491cc6b96eb9f9006e1d1b041572638b4c65
Varun Panchal <varunpanchal1008@gmail.com>
2026-05-02 15:17:13 +0530

    update

--- db.py
+++ db.py
@@ -1,19 +1,13 @@
-
-
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-import os
 
-DATABASE_URL = "mysql+pymysql://3fJbKsdTZJN4psR.root:P4rWNqS9tDrBr5vm@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/test?ssl_ca=<CA_PATH>&ssl_verify_cert=true&ssl_verify_identity=true"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
-    connect_args={
-        "ssl": {
-            "ca": "C:/projects/Nexlanc Ai/ca.pem"   
-        }
-    }
+    pool_recycle=280
 )
 
 SessionLocal = sessionmaker(bind=engine)
